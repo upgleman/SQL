91ee2e08ad52fae17d0dc71854bd92586e74adbb
upgleman <upgleman@nate.com>
2022-08-24 10:19:50 +0900

    #SQL2 실력테스트 최종

--- SQL2Test.sql
+++ SQL2Test.sql
@@ -5,6 +5,7 @@
 DESC USER;
 
 -- 미국 유저들에 대해 잔액정보 내림차순 기준으로 3개 그룹으로 나누어 데이터를 조회하세요.
+--TIP) 미국 유저에 대한 조건은 WHERE문을 이용하고, 그룹을 나누기 위해서는 NTILE 함수를 이용해봅시다.
 
 select * from USER;
 
@@ -65,6 +66,8 @@ DESC USER_LINE;
 DESC USER_KAKAO;
 
 -- 정상적으로 가입된 유저에 대해 user_id와 user_email를 모두 출력해보세요.
+--TIP) WHERE문을 통해 valid값이 true인 유저정보를 추출하는 2개의 SELECT문에 UNION집합연산자를 이용해보세요.
+
 select * from USER_LINE;
 select * from USER_KAKAO;
 
@@ -143,6 +146,7 @@ DESC EMPLOYEE;
 DESC EMPLOYEE_PERFORMANCE;
 
 -- 두 테이블을 결합하여 성과 포인트를 기준으로 순위를 매겨 지시사항에서 요구하는 정보를 조회하세요.
+--TIP) EMPLOYEE테이블의 id와 EMPLOYEE_PERFORMANCE테이블의 employee_id를 기준으로 INNER JOIN을 하고 performance_point 내림차순 기준으로 DENSE_RANK 함수를 활용해보세요.
 
 select * from EMPLOYEE;
 select * from EMPLOYEE_PERFORMANCE;
@@ -219,6 +223,7 @@ DESC DOCUMENT_TYPE;
 DESC EMPLOYEE;
 
 -- 기밀 등급이 Top Secret이고 사원이 작성한 문서들의 정보를 출력하세요.
+--TIP) 지시사항에 안내된 컬럼들을 통해 2개의 JOIN을 실시해봅시다.
 
 select * from DOCUMENT;
 select * from DOCUMENT_TYPE;
@@ -303,6 +308,7 @@ DESC SUPERMARKET;
 DESC DEPARTMENT_STORE;
 
 -- SUPERMARKET과 DEPARTMENT_STORE에서 취급되는 모든 상품을 조회하세요.
+--TIP) FULL OUTER JOIN을 구현하기 위해 LEFT OUTER JOIN과 RIGHT OUTER JOIN, 집합연산자 UNION을 활용해봅시다.
 
 select * from SUPERMARKET;
 select * from DEPARTMENT_STORE;
@@ -377,6 +383,7 @@ order by product_id asc;
 DESC MEMBER;
 
 -- MEMBER 테이블로 계층형질의를 실시하고 lvl, member_name, manager_name을 조회하세요.
+--TIP) 계층형 질의를 통한 CTE 테이블을 기준으로 MEMBER테이블과 LEFT OUTER JOIN을 해봅시다
 
 select * from MEMBER;
 
@@ -462,11 +469,245 @@ ORDER BY CTE.member_id ASC;
 
 -- Q7.분점들의 이벤트
 -- 지시사항 1번을 참고하여 코드를 작성하세요.
+-- 아래는 BRANCH, EVNET_HISTORY, TARGET_BRANCH 테이블에 대한 구조를 보여줍니다. 테이블을 수정하실 필요는 없습니다.
+DESC BRANCH;
+DESC EVENT_HISTORY;
+DESC TARGET_BRANCH;
+
+-- 아직 체크되지 않은 분점들 중에서 sale 이벤트를 개최한 분점 정보를 출력하세요.
+ --TIP) 비연관 서브쿼리를 통해 타겟 분점 ID를 구하여 정보를 출력해보세요.
+ select * from BRANCH;
+ select * from EVENT_HISTORY;
+ select * from TARGET_BRANCH;
+ 
+ select BRANCH.branch_id, branch_name,owner_name,event_type,event_dtl
+from BRANCH
+inner join EVENT_HISTORY
+on BRANCH.branch_id = EVENT_HISTORY.branch_id
+where  event_type = 'sale' and
+BRANCH.branch_id = ANY (
+    select branch_id 
+    from TARGET_BRANCH
+    where checked = FALSE
+)
+order by branch_id asc;
+
++-------------+--------------+------+-----+---------+-------+
+| Field       | Type         | Null | Key | Default | Extra |
++-------------+--------------+------+-----+---------+-------+
+| branch_id   | int(11)      | NO   | PRI | NULL    |       |
+| branch_name | varchar(100) | YES  |     | NULL    |       |
+| owner_name  | varchar(100) | YES  |     | NULL    |       |
++-------------+--------------+------+-----+---------+-------+
++------------+--------------+------+-----+---------+-------+
+| Field      | Type         | Null | Key | Default | Extra |
++------------+--------------+------+-----+---------+-------+
+| event_id   | int(11)      | NO   | PRI | NULL    |       |
+| branch_id  | int(11)      | YES  |     | NULL    |       |
+| event_type | varchar(20)  | YES  |     | NULL    |       |
+| event_dtl  | varchar(100) | YES  |     | NULL    |       |
++------------+--------------+------+-----+---------+-------+
++-----------+------------+------+-----+---------+-------+
+| Field     | Type       | Null | Key | Default | Extra |
++-----------+------------+------+-----+---------+-------+
+| target_id | int(11)    | NO   | PRI | NULL    |       |
+| branch_id | int(11)    | YES  |     | NULL    |       |
+| checked   | tinyint(1) | YES  |     | NULL    |       |
++-----------+------------+------+-----+---------+-------+
++-----------+-------------+------------+
+| branch_id | branch_name | owner_name |
++-----------+-------------+------------+
+|         1 | branch 01   | kim        |
+|         2 | branch 02   | lee        |
+|         3 | branch 03   | choi       |
+|         4 | branch 04   | moon       |
+|         5 | branch 05   | park       |
+|         6 | branch 06   | song       |
+|         7 | branch 07   | bae        |
+|         8 | branch 08   | ko         |
+|         9 | branch 09   | koo        |
+|        10 | branch 10   | jeon       |
++-----------+-------------+------------+
++----------+-----------+------------+-------------+
+| event_id | branch_id | event_type | event_dtl   |
++----------+-----------+------------+-------------+
+|        1 |         3 | open       | open event! |
+|        2 |         1 | open       | open event! |
+|        3 |         7 | sale       | sale event! |
+|        4 |         5 | sale       | sale event! |
+|        5 |         2 | open       | open event! |
+|        6 |         1 | sale       | sale event! |
+|        7 |         3 | sale       | sale event! |
++----------+-----------+------------+-------------+
++-----------+-----------+---------+
+| target_id | branch_id | checked |
++-----------+-----------+---------+
+|         1 |         1 |       0 |
+|         2 |         2 |       1 |
+|         3 |         3 |       1 |
+|         4 |         5 |       1 |
+|         5 |         6 |       0 |
+|         6 |         7 |       0 |
+|         7 |         8 |       0 |
+|         8 |         9 |       0 |
+|         9 |        10 |       0 |
++-----------+-----------+---------+
++-----------+-------------+------------+------------+-------------+
+| branch_id | branch_name | owner_name | event_type | event_dtl   |
++-----------+-------------+------------+------------+-------------+
+|         1 | branch 01   | kim        | sale       | sale event! |
+|         7 | branch 07   | bae        | sale       | sale event! |
++-----------+-------------+------------+------------+-------------+
 
 -- Q8.코인 가격 차이
+-- 아래는 KOREA_EXCHANGE, JAPAN_EXCHANGE 테이블에 대한 구조를 보여줍니다. 테이블을 수정하실 필요는 없습니다.
+DESC KOREA_EXCHANGE;
+DESC JAPAN_EXCHANGE;
 
+-- 코인 티커가 동일한 코인에 대해서 한국 거래소 가격이 더 싸거나 같은 가격인 코인을 출력하세요.
+--연관 서브쿼리를 이용하여, 코인 티커는 일치하지만 한국 거래소의 가격이 더 싸거나 같다는 조건을 만들어보세요.
+
+select * from KOREA_EXCHANGE;
+select * from JAPAN_EXCHANGE;
+
+select KOREA_EXCHANGE.coin_ticker ,KOREA_EXCHANGE.price
+from KOREA_EXCHANGE
+where price <= ANY (
+    select price
+    from JAPAN_EXCHANGE
+    where KOREA_EXCHANGE.coin_ticker = JAPAN_EXCHANGE.coin_ticker
+)
+order by coin_ticker asc;
+
++----+-------------+----------+
+| id | coin_ticker | price    |
++----+-------------+----------+
+|  1 | AAA         |      100 |
+|  2 | BBB         |      200 |
+|  3 | CCC         |     1000 |
+|  4 | DDD         |   500000 |
+|  5 | EEE         |   200000 |
+|  6 | FFF         |     6500 |
+|  7 | GGG         |    78000 |
+|  8 | HHH         |  1000000 |
+|  9 | III         |     3600 |
+| 10 | JJJ         |       10 |
+| 11 | KKK         | 80000000 |
+| 12 | LLL         |    40000 |
+| 13 | MMM         |        5 |
+| 14 | NNN         |     1000 |
++----+-------------+----------+
++----+-------------+----------+
+| id | coin_ticker | price    |
++----+-------------+----------+
+|  1 | AAA         |      110 |
+|  2 | BBB         |      300 |
+|  3 | CCC         |     1000 |
+|  4 | DDD         |   480000 |
+|  5 | EEE         |   100000 |
+|  6 | FFF         |     6600 |
+|  7 | GGG         |    78100 |
+|  8 | HHH         |   980000 |
+|  9 | III         |     3300 |
+| 10 | JJJ         |        8 |
+| 11 | KKK         | 79950000 |
+| 12 | LLL         |    30000 |
+| 13 | MMM         |        4 |
+| 14 | NNN         |     1000 |
++----+-------------+----------+
++-------------+-------+
+| coin_ticker | price |
++-------------+-------+
+| AAA         |   100 |
+| BBB         |   200 |
+| CCC         |  1000 |
+| FFF         |  6500 |
+| GGG         | 78000 |
+| NNN         |  1000 |
++-------------+-------+
 
 -- Q9.예약자 상세정보
+-- 아래는 MEMBER, MEMBER_DTL, RESERVATION 테이블에 대한 구조를 보여줍니다. 테이블을 수정하실 필요는 없습니다.
+DESC MEMBER;
+DESC MEMBER_DTL;
+DESC RESERVATION;
 
+-- 예약을 취소하지 않은 멤버들의 상세 정보를 출력하세요.
+--INNER JOIN과 EXISTS를 활용하여 예약을 취소하지 않은 멤버의 상세 정보를 출력해보세요.
 
--- 3. 위에서 작성한 두 쿼리에서 ORDER BY를 제거 후, 중복을 제거하는 집합연산자로 결합하고 최종 결과에 대해 정렬을 해주세요.
+select * from MEMBER;
+select * from MEMBER_DTL;
+select * from RESERVATION;
+
+select MEMBER.member_id, MEMBER.member_name, number, email
+from MEMBER
+inner join MEMBER_DTL
+on MEMBER.member_id = MEMBER_DTL.member_id
+where EXISTS (
+    select member_id
+    from RESERVATION
+    where canceled=FALSE 
+    and MEMBER.member_id=RESERVATION.member_id
+);
+
++-------------+--------------+------+-----+---------+-------+
+| Field       | Type         | Null | Key | Default | Extra |
++-------------+--------------+------+-----+---------+-------+
+| member_id   | int(11)      | NO   | PRI | NULL    |       |
+| member_name | varchar(100) | YES  |     | NULL    |       |
++-------------+--------------+------+-----+---------+-------+
++-----------+--------------+------+-----+---------+-------+
+| Field     | Type         | Null | Key | Default | Extra |
++-----------+--------------+------+-----+---------+-------+
+| member_id | int(11)      | NO   | PRI | NULL    |       |
+| number    | varchar(100) | YES  |     | NULL    |       |
+| email     | varchar(100) | YES  |     | NULL    |       |
++-----------+--------------+------+-----+---------+-------+
++----------------+------------+------+-----+---------+-------+
+| Field          | Type       | Null | Key | Default | Extra |
++----------------+------------+------+-----+---------+-------+
+| reservation_id | int(11)    | NO   | PRI | NULL    |       |
+| member_id      | int(11)    | YES  |     | NULL    |       |
+| canceled       | tinyint(1) | YES  |     | NULL    |       |
++----------------+------------+------+-----+---------+-------+
++-----------+-------------+
+| member_id | member_name |
++-----------+-------------+
+|         1 | Kim         |
+|         2 | Lee         |
+|         3 | Park        |
+|         4 | Choi        |
+|         5 | Song        |
+|         6 | Koo         |
+|         7 | Moon        |
+|         8 | Bae         |
++-----------+-------------+
++-----------+---------------+---------------+
+| member_id | number        | email         |
++-----------+---------------+---------------+
+|         1 | 010-1111-1111 | aaa@naver.com |
+|         2 | 010-2222-2222 | bbb@naver.com |
+|         3 | 010-3333-3333 | ccc@naver.com |
+|         4 | 010-4444-4444 | ddd@naver.com |
+|         5 | 010-5555-5555 | eee@naver.com |
+|         6 | 010-6666-6666 | fff@naver.com |
+|         7 | 010-7777-7777 | ggg@naver.com |
+|         8 | 010-8888-8888 | hhh@naver.com |
++-----------+---------------+---------------+
++----------------+-----------+----------+
+| reservation_id | member_id | canceled |
++----------------+-----------+----------+
+|              1 |         1 |        0 |
+|              2 |         3 |        1 |
+|              3 |         4 |        0 |
+|              4 |         6 |        0 |
+|              5 |         7 |        0 |
++----------------+-----------+----------+
++-----------+-------------+---------------+---------------+
+| member_id | member_name | number        | email         |
++-----------+-------------+---------------+---------------+
+|         1 | Kim         | 010-1111-1111 | aaa@naver.com |
+|         4 | Choi        | 010-4444-4444 | ddd@naver.com |
+|         6 | Koo         | 010-6666-6666 | fff@naver.com |
+|         7 | Moon        | 010-7777-7777 | ggg@naver.com |
++-----------+-------------+---------------+---------------+
